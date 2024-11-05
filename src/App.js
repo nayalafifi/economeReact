8ef38f44ce3da3581ee1fed8939fefed4e13b112
NAYA ALAFIFI <na3266@nyu.edu>
2024-11-05 00:14:26 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import './App.css';
+import React, { useState } from 'react';  // Import React and useState
+import Dashboard from './dashboard';      // Import Dashboard component
+import Profile from './Profile';          // Import Profile component
+import GoalSetting from './GoalSetting';  // Import GoalSetting component
+import './App.css';                       // Import CSS
 
 function App() {
   const [currentPage, setCurrentPage] = useState('dashboard');
@@ -7,6 +11,7 @@ function App() {
     <div className="App">
       {currentPage === 'dashboard' && <Dashboard navigateTo={setCurrentPage} />}
       {currentPage === 'profile' && <Profile />}
+      {currentPage === 'GoalSetting' && <GoalSetting />}
     </div>
   );
 }
