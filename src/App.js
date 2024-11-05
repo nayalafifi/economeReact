e2f1b6c12d849e51091253d5a818c9e74555bea4
izah2587 <is2587@nyu.edu>
2024-11-04 22:34:33 -0500

    dashboard.js created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Dashboard from './dashboard';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* Redirect root path to /dashboard */}
+          <Route path="/" element={<Navigate to="/index" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
